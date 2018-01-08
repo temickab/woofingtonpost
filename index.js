29403d26131120671b778e27a55804ca703ec134
Temicka Brown <mekapleio@yahoo.com>
2018-01-08 18:45:48 -0500

    messed something up, searching for the issue

--- index.js
+++ index.js
@@ -7,29 +7,29 @@ const bodyParser = require('body-parser')
 
 //For example, use the following code to 
 //serve images, CSS files, and JavaScript files in a directory named public:
-app.use(express.static('public'));
+app.use('/assets', express.static('public'))
 
 
-app.engine('handlebars',exhbs( { defaultLayout: 'main' })) 
-app.set('view engine', 'handlebars') 
+app.engine('exhbs', hbs({ defaultLayout: 'main' })) 
+app.set('view engine', 'exhbs')
 
 app.listen(1986, () => {
     console.log("app listening on port 1986") //1986 is the year i was born
   })
 
-  //Routing
-  app.get('/', (req, res) => {
-    res.render('homepg')
- })
-
- app.get('/articles', (req, res) => {
-     res.render('articles') 
- })
-
+    //Routing
+    app.get('/', (req, res) => {
+        res.render('homepg')
+     })
+    
+     app.get('/articles', (req, res) => {
+         res.render('articles') 
+     })
+    
 
   /*app.get("/:name", (req, res) => {
     res.send(`hello ${req.params.name}`)
-  })
+  })*/
 
 
 
