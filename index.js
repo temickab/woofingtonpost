3835a0bcf97f87847728497732310616ec6c88d5
Temicka Brown <mekapleio@yahoo.com>
2018-01-08 19:34:26 -0500

    added a background photo, Photo by Mat Hayward/Shutterstock and did some css changes

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 const express = require('express')
-const app = express()
 const exhbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const app = express()
 //
 
 
 //For example, use the following code to 
 //serve images, CSS files, and JavaScript files in a directory named public:
-app.use('/assets', express.static('public'))
+app.use(express.static('public'))
 
 
-app.engine('exhbs', hbs({ defaultLayout: 'main' })) 
-app.set('view engine', 'exhbs')
+app.engine('handlebars',exhbs( { defaultLayout: 'main' })) 
+app.set('view engine', 'handlebars') 
 
 app.listen(1986, () => {
-    console.log("app listening on port 1986") //1986 is the year i was born
+    console.log("The best year ever is 1986") //1986 is the year i was born
   })
 
-    //Routing
-    app.get('/', (req, res) => {
-        res.render('homepg')
-     })
-    
-     app.get('/articles', (req, res) => {
-         res.render('articles') 
-     })
-    
+   //Routing
+   app.get('/', (req, res) => {
+    res.render('homepg')
+ })
+
+ app.get('/articles', (req, res) => {
+    res.render('articles') 
+})
+
+
+
 
   /*app.get("/:name", (req, res) => {
     res.send(`hello ${req.params.name}`)
